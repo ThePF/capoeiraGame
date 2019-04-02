12477efd1bc2a5e573ae07e330699afa06065323
Theresa Pancini Fitzek <theresapancini@gmx.de>
2019-04-02 19:01:48 +0200

    all keys are set false while clicking a new one

--- scripts/fighter.js
+++ scripts/fighter.js
@@ -18,7 +18,7 @@ let state = {
 //Create classes for fighter//
 
 function renderEverything() {
-  console.log('Hello2');
+  // console.log('Hello2');
   renderKick1();
   renderKick2();
   renderKick3();
@@ -28,60 +28,61 @@ function renderEverything() {
 }
 
 document.addEventListener('keydown', evt => {
+  Object.keys(state).forEach(v => (state[v] = false));
   switch (evt.code) {
-    case 65:
-      state.kick1 = true;
+    case 'KeyA':
+      state.kick1 = !state.kick1;
       break;
-    case 83:
-      state.kick2;
+    case 'KeyS':
+      state.kick2 = !state.kick2;
       break;
-    case 68:
-      state.kick3;
+    case 'KeyD':
+      state.kick3 = !state.kick3;
       break;
-    case 70:
+    case 'KeyF':
       state.kick4;
       break;
-    case 71:
+    case 'KeyG':
       state.kick5;
       break;
-    case 72:
+    case 'KeyH':
       state.kick6;
       break;
   }
-  console.log(evt.code);
+  console.log(state);
   renderEverything();
 });
 
 const fighter = document.querySelector('.fighter');
 
 function renderKick1() {
-  if (state.kick1) {
-    console.log('hello');
-    figther.classList.remove('fighter');
-    fighter.classList.add('kick1');
+  if (state.kick1 === true) {
+    fighter.style.backgroundImage = "url('/picsMovements/Kicks/K-Fighter/Meia Lua de frente.png')";
   } else {
-    fighter.classList.remove('kick1');
+    fighter.style.backgroundImage = "url('/picsMovements/Ginga-F.png')";
   }
 }
 
 function renderKick2() {
-  if (state.kick1) {
-    fighter.classList.add('kick2');
+  if (state.kick2 === true) {
+    fighter.style.backgroundImage = "url('/picsMovements/Kicks/K-Fighter/Martelo.png')";
   } else {
-    fighter.classList.remove('kick2');
+    fighter.style.backgroundImage = "url('/picsMovements/Ginga-F.png')";
   }
 }
 
 function renderKick3() {
-  if (state.kick1) {
-    fighter.classList.add('kick3');
-  } else {
-    fighter.classList.remove('kick3');
-  }
+  // if (state.kick3 === true) {
+  //   fighter.style.backgroundImage = "url('/picsMovements/Kicks/K-Fighter/Macaco.png')";
+  //   // fighter.style.width = '500px';
+  // } else {
+  //   fighter.style.backgroundImage = "url('/picsMovements/Ginga-F.png')";
+  //   // fighter.style.width = '350px;';
+  // }
 }
 
 function renderKick4() {
-  if (state.kick1) {
+  if (state.kick4) {
     fighter.classList.add('kick4');
   } else {
     fighter.classList.remove('kick4');
@@ -89,7 +90,7 @@ function renderKick4() {
 }
 
 function renderKick5() {
-  if (state.kick1) {
+  if (state.kick5) {
     fighter.classList.add('kick5');
   } else {
     fighter.classList.remove('kick5');
@@ -97,7 +98,7 @@ function renderKick5() {
 }
 
 function renderKick6() {
-  if (state.kick1) {
+  if (state.kick6) {
     fighter.classList.add('kick6');
   } else {
     fighter.classList.remove('kick6');
